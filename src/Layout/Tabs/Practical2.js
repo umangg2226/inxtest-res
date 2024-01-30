65c31e49697de750fa60f85a58a961be18f52079
umangg2226 <umang2226@gmail.com>
2024-01-30 23:51:54 +0530

    hover transition + dynamic tooltip

--- src/Layout/Tabs/Practical2.js
+++ src/Layout/Tabs/Practical2.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Box, Grid } from '@mui/material'
+import { Box, Grid, Fade } from '@mui/material'
 
 const data = [
   Array(3).fill('header'),
@@ -7,6 +7,40 @@ const data = [
   Array(3).fill('row'),
   Array(3).fill('row'),
 ]
+
+const Practical2 = () => {
+  return (
+    <Grid container spacing={7}>
+      {data.map((list, i) => {
+        return (
+          <Grid item xs={12} key={i}>
+            <Grid container spacing={7}>
+              {list.map((name, index) => {
+                const previousLength = data.reduce((total, next, idx) => {
+                  if (idx < i) total += next.length
+                  return total
+                }, 0)
+
+                const tooltipMessage =
+                  name === 'header'
+                    ? `Tooltip ${index + 1}`
+                    : `Tooltip ${previousLength + index + 1}`
+
+                return (
+                  <Message
+                    key={index}
+                    tooltipMessage={tooltipMessage}
+                    value={`${name} ${name === 'header' ? index + 1 : i}`}
+                  />
+                )
+              })}
+            </Grid>
+          </Grid>
+        )
+      })}
+    </Grid>
+  )
+}
 
 const Message = ({ tooltipMessage, value }) => {
   const [isFocused, setIsFocused] = useState(false)
@@ -25,10 +59,18 @@ const Message = ({ tooltipMessage, value }) => {
           border: '1px solid #fff',
           color: '#fff',
           position: 'relative',
+          boxShadow: isFocused ? '0px 0px 19px 1px rgba(0,0,0,0.2)' : '',
+          transition: '300ms',
         }}
       >
         {value}
-        {isFocused ? (
+        <Fade
+          in={isFocused}
+          timeout={{
+            enter: 300,
+            exit: 300,
+          }}
+        >
           <Box
             style={{
               position: 'absolute',
@@ -44,38 +86,8 @@ const Message = ({ tooltipMessage, value }) => {
           >
             {tooltipMessage}
           </Box>
-        ) : (
-          ''
-        )}
+        </Fade>
       </Box>
-    </Grid>
-  )
-}
-
-const Practical2 = () => {
-  return (
-    <Grid container spacing={7}>
-      {data.map((list, i) => {
-        return (
-          <Grid item xs={12} key={i}>
-            <Grid container spacing={7}>
-              {list.map((name, index) => {
-                const tooltipMessage =
-                  name === 'header'
-                    ? `Tooltip ${index + 1}`
-                    : `Tooltip ${list?.[i - 1].length * i + index + 1}`
-
-                return (
-                  <Message
-                    tooltipMessage={tooltipMessage}
-                    value={`${name} ${name === 'header' ? index + 1 : i}`}
-                  />
-                )
-              })}
-            </Grid>
-          </Grid>
-        )
-      })}
     </Grid>
   )
 }
